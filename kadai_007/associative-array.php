f180579d8f9dad29c889a131c27f8ba4e24f797a
mi95h027 <mymi95h027@gmail.com>
2026-01-28 19:40:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lan="ja">
+
+<head>
+  <p>
+    <?php
+    //連想配列に値を代入する
+    $product_data = ['name' => 'onion' , 'price' => '200' , 'weight' => '180'];
+
+    //連想配列を週力する
+    print_r($product_data);
+    ?>
+  </p>
+</head>
+
+</html>
