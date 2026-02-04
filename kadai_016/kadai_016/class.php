1451288365e3912da11f2407f34507d75f19c3f0
mi95h027 <mymi95h027@gmail.com>
2026-02-04 18:51:45 +0900

    Add files via upload

---
+++ kadai_016/kadai_016/class.php
@@ -0,0 +1,50 @@
+<?php
+//クラスを定義する
+class Food {
+     //プロパティを定義する
+     private $name;
+     private $price;
+
+     //コンストラクタを定義する
+     public function __construct(string $name, int $price) {
+         $this->name = $name;
+         $this->price = $price;
+     }
+     // メソッドを定義する
+    public function show_price() {
+        echo $this->price;
+    }
+}
+     //インスタンス化する
+     $food = new Food('potato', 250);
+
+
+     //値を出力する
+     print_r($food);
+
+//クラスを定義する
+class Animal {
+     //プロパティを定義する
+     private $name;
+     private $height;
+     private $weight;
+
+     //コンストラクタを定義する
+     public function __construct(string $name, int $height, int $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+     }
+     // メソッドを定義する
+     public function show_height() {
+         echo $this->height;
+    }
+}    
+        //インスタンス化する
+       $animal = new Animal('dog', 60, 5000);
+
+      //値を出力する
+      print_r($animal);
+?>
+     
+
